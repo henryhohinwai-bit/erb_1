5c9e6c131c973f7e9b490d83f0c7b7379536b759
henryhohinwai@gmail.com <henryhohinwai@gmail.com>
2026-08-05 21:14:04 +0800

    feat: migrate ERB_1 from SQLite to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'erb_1',
+        'USER': 'henryho',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
